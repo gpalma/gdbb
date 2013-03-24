8acaa56fa833b4bed4ab896a8aa7b5fc2bfe1336
josegpg <gregorio.pinero07@gmail.com>
2013-03-24 13:29:16 -0430

    Update GraphDB.java
    Cambie firma de getInDegree y getOutDegree y agregue la implementacion
    de Subgraph y de print ya que son la misma para todos

--- ve/usb/gdbb/GraphDB.java
+++ ve/usb/gdbb/GraphDB.java
@@ -18,7 +18,12 @@
 
 package ve.usb.gdbb;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public abstract class GraphDB implements Graph {
 	protected int V;
@@ -36,8 +41,68 @@ public abstract class GraphDB implements Graph {
 	public abstract Iterator<String> adj(String nodeId);
 	public abstract Iterator<Edge> getEdges();
 	public abstract Iterator<String> getNodes();
-	public abstract Iterator<Integer> getInDegree();
-	public abstract Iterator<Integer> getOutDegree();
+	public abstract Integer getInDegree(String nodeId);
+	public abstract Integer getOutDegree(String nodeId);
+        
+        /*
+     * Funcion que retorna un subgrafo del grafo actual.
+     */
+        public Graph subGraph(int n) {
+            /* Choosing a random node, to generate
+                * a subgraph from it's BFS run
+                */
+            Iterator<Edge> iter = this.getEdges();
+            // This is not random FIX
+            Edge randomEdge = (Edge) iter.next();
+            String randomNode = randomEdge.getSrc();
+            int totalNodes = 1;
+            /* BFS run */
+            Graph subGraph = new DiGraphAdjList();
+            Queue<String> queue = new LinkedList<String>();
+            HashSet<String> visitedNodes = new HashSet<String>();
+            Iterator<String> adjActual;
+            String next;
+            String aux = randomNode;
+            queue.add(aux);
+
+            subGraph.addNode(aux);
+            visitedNodes.add(aux);
+
+            while(!queue.isEmpty() && totalNodes<n) {
+                    aux = queue.poll();
+                    adjActual = this.adj(aux);
+                    while(adjActual.hasNext()) {
+                            next = adjActual.next();
+                            if (!visitedNodes.contains(next) && totalNodes<n) {
+                                    queue.add(next);
+                                    subGraph.addNode(next);
+                                    visitedNodes.add(next);
+                                    subGraph.addEdge(new Edge(totalNodes+"", aux, next));
+                                    totalNodes++;
+                            }
+                    }
+            }
+
+        return subGraph;
+	}
+	
+	/*Funcion que,dado el String File, imprime en un archivo, cuyo nombre
+	 *sera File, el grafo en formato SIF*/
+	public void print(String File) {
+            try{
+                FileWriter fstream = new FileWriter(File);
+                BufferedWriter out = new BufferedWriter(fstream);
+                Iterator<Edge> archs = this.getEdges();
+                Edge curr;
+                while ( archs.hasNext() ) {
+                        curr = archs.next();
+                        out.write(curr.getSrc()+"\tpr\t"+curr.getDst()+"\n");
+                }
+                out.close();
+            }catch (Exception e){//Catch exception if any
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
 	
 	public abstract boolean patternMatching(Graph subGraph);
 }
