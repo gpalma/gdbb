90208327da8cb3865397ca8ce012a0a261f3b3d4
Alejandro Flores V. <alejandroflores.af@gmail.com>
2014-01-19 18:52:33 -0430

    Create TestGraphSummarization.java

---
+++ testgdbb/TestGraphSummarization.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (C) 2013, Universidad Simon Bolivar
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package testgdbb;
+import java.util.ArrayList;
+import java.util.Queue;
+import ve.usb.gdbb.*;
+
+public class TestGraphSummarization extends TestNP {
+
+	public TestGraphSummarization() {
+		TestName = "Graph Summarization";
+	}
+	
+	protected boolean testGraph(){
+        String[] nodos;
+        GraphSummarization s;
+        DiGraphAdjList g;
+        nodos = this.nextRandomNode(N);
+        if(nodos == null) return false;
+        for(int i = 0; i < N; i++){
+            g = getGraph(nodos[i]);
+            s = new GraphSummarization();
+            s.Summarize(g);
+            g = (DiGraphAdjList)s.returnGSummarize();
+        }
+        return true;
+    }
+}
