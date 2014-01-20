5e8fa930636ed5890eb30add85055438375ccdab
Alejandro Flores V. <alejandroflores.af@gmail.com>
2014-01-19 21:40:42 -0430

    Update ShortPath.java

--- ve/usb/gdbb/ShortPath.java
+++ ve/usb/gdbb/ShortPath.java
@@ -21,101 +21,107 @@ package ve.usb.gdbb;
 import java.util.ArrayList;
 import java.util.TreeSet;
 import java.util.Iterator;
+
 public class ShortPath {
-    TreeSet <String> resultado;
-    
-    /*
-     * Constructor de la clase
-     */
-    public ShortPath(){
-    }
-    
-    /*
-     * Funcion que devuelve el camino mas corto desde v1 hasta v2 en 
-     * un arraylist que muestra los nodos intermedios partiendo desde v1 
-     * hasta v2.
-     * Retorna NULL en caso de no encontrar camino.
-     */
-    public ArrayList<String> getPath(Graph g, String v1, String v2) {
-        ArrayList<String> res = new ArrayList<String>();
-        System.out.println(g.V());
-        for(int i = 0; i < g.V(); i++){
-            
-            if(DFScamino(g, 0, i, v1, v2, res)){
-                return res;
-            }
-        }
-        return null;
-    }
-    /*
-     * Funcion que realiza una busqueda con DFS usando una cota.
-     */
-    private boolean DFScamino(Graph g, int i, int cota, String actual, 
-                              String objetivo, ArrayList<String> res){
-        if(actual.equals(objetivo)){ 
-            res.add(i, actual);
-            return true;
-        }
-        if(i >= cota) return false;
-        Iterator<String> siguientes = g.adj(actual);
-        while(siguientes.hasNext()){
-            if(DFScamino(g, i+1, cota, siguientes.next(), objetivo, res)){
-                res.add(i, actual);
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    
-    /*
-     * Funcion que devuelve la distancia mas corta entre dos nodos v1 y v2.
-     * Si no encuentra camino retorna -1.
-     */
-    public int getDistance(Graph g, String v1, String v2) {
-        for(int i = 0; i < g.V(); i++){
-            if(DFS(g, 0, i, v1, v2)) return i;
-        }
-        return -1;
-    }
-    /*
-     * Funcion que realiza una busqueda con DFS usando una cota.
-     */
-    private boolean DFS(Graph g, int i, int cota, String actual, String objetivo){
-        if(actual.equals(objetivo)){
-            return true;
-        }
-        if(i >= cota) return false;
-        Iterator<String> siguientes = g.adj(actual);
-        while(siguientes.hasNext())
-            if(DFS(g, i+1, cota, siguientes.next(), objetivo))
-                return true;
-        return false;
-    }
-    
-    /*
-     * Return all nodes khops from src
-     */
-    public Iterator<String> khops(Graph g, String src, int k){
-        resultado = new TreeSet<String>();
-        DFS(g, 0, k, src);
-        return resultado.iterator();
-    }
-    
-    /*
-     * Funcion que realiza una busqueda con DFS usando una cota.
-     */
-    private boolean DFS(Graph g, int i, int cota, String actual){
-        if(i >= cota){
-            resultado.add(actual);
-            return true;
-        }
-        Iterator<String> siguientes = g.adj(actual);
-        while(siguientes.hasNext()){
-            DFS(g, i+1, cota, siguientes.next());
-        }
-        return true;
-    }
-    
-    
+
+	TreeSet<String> resultado;
+
+	/*
+	 * Constructor de la clase
+	 */
+	public ShortPath() {}
+
+	/*
+	 * Funcion que devuelve el camino mas corto desde v1 hasta v2 en 
+	 * un arraylist que muestra los nodos intermedios partiendo desde v1 
+	 * hasta v2.
+	 * Retorna NULL en caso de no encontrar camino.
+	 */
+	public ArrayList<String> getPath(Graph g, String v1, String v2) {
+		ArrayList<String> res = new ArrayList<String>();
+		System.out.println(g.V());
+		for(int i = 0; i < g.V(); i++){
+			if(DFScamino(g, 0, i, v1, v2, res)){
+				return res;
+			}
+		}
+		return null;
+	}
+	/*
+	 * Funcion que realiza una busqueda con DFS usando una cota.
+	 */
+	private boolean DFScamino(Graph g, int i, int cota, String actual, 
+			String objetivo, ArrayList<String> res)
+	{
+		if (actual.equals(objetivo)) {
+			res.add(i, actual);
+			return true;
+		}
+		if (i >= cota) return false;
+		GraphIterator<String> siguientes = g.adj(actual);
+		while (siguientes.hasNext()) {
+			if (DFScamino(g, i+1, cota, siguientes.next(), objetivo, res)) {
+				res.add(i, actual);
+				siguientes.close();
+				return true;
+			}
+		}
+		siguientes.close();
+		return false;
+	}
+
+
+	/*
+	 * Funcion que devuelve la distancia mas corta entre dos nodos v1 y v2.
+	 * Si no encuentra camino retorna -1.
+	 */
+	public int getDistance(Graph g, String v1, String v2) {
+		for (int i = 0; i < g.V(); i++) {
+			if (DFS(g, 0, i, v1, v2)) return i;
+		}
+		return -1;
+	}
+	/*
+	 * Funcion que realiza una busqueda con DFS usando una cota.
+	 */
+	private boolean DFS(Graph g, int i, int cota, String actual, String objetivo){
+		if (actual.equals(objetivo)) {
+			return true;
+		}
+		if (i >= cota) return false;
+		GraphIterator<String> siguientes = g.adj(actual);
+		while (siguientes.hasNext()) {
+			if (DFS(g, i+1, cota, siguientes.next(), objetivo)) {
+				siguientes.close();
+				return true;
+			}
+		}
+		siguientes.close();
+		return false;
+	}
+
+	/*
+	 * Return all nodes khops from src
+	 */
+	public Iterator<String> khops(Graph g, String src, int k){
+		resultado = new TreeSet<String>();
+		DFS(g, 0, k, src);
+		return resultado.iterator();
+	}
+
+	/*
+	 * Funcion que realiza una busqueda con DFS usando una cota.
+	 */
+	private boolean DFS(Graph g, int i, int cota, String actual){
+		if(i >= cota){
+			resultado.add(actual);
+			return true;
+		}
+		GraphIterator<String> siguientes = g.adj(actual);
+		while(siguientes.hasNext()){
+			DFS(g, i+1, cota, siguientes.next());
+		}
+		siguientes.close();
+		return true;
+	}
 }
